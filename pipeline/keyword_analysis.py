e5b3ccfdc07456117aad22144b6ce7a78c11318a
Sowdeshwar Survesha Kumaar <59279838+Sowdeshwar-99@users.noreply.github.com>
2026-03-14 20:25:18 +1000

    Create keyword_analysis.py

---
+++ pipeline/keyword_analysis.py
@@ -0,0 +1,19 @@
+from pyspark.sql.functions import col
+
+def keyword_frequency(word_df):
+
+    word_count = word_df.groupBy("word") \
+        .count() \
+        .orderBy("count", ascending=False)
+
+    return word_count
+
+
+def keyword_spending(join_spend_clean):
+
+    word_spend = join_spend_clean.groupBy("word") \
+        .sum("spend_amt") \
+        .withColumnRenamed("sum(spend_amt)", "total_spend") \
+        .orderBy("total_spend", ascending=False)
+
+    return word_spend
