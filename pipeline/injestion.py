78f1190a6266d5af5645e5c99df82b595c78671d
Sowdeshwar Survesha Kumaar <59279838+Sowdeshwar-99@users.noreply.github.com>
2026-03-14 20:31:23 +1000

    Update injestion.py

--- pipeline/injestion.py
+++ pipeline/injestion.py
@@ -1,8 +1,17 @@
 from pyspark.sql import SparkSession
 
 def create_spark():
-    return SparkSession.builder.appName("FacebookAdsAnalytics").getOrCreate()
+    spark = SparkSession.builder.appName(
+        "Facebook Political Ads Analytics"
+    ).getOrCreate()
+    return spark
+
 
 def load_ads_data(spark, path):
-    df = spark.read.option("multiLine", "false").json(path)
+    df = spark.read.option(
+        "multiLine", "false"
+    ).json(path)
+
+    df.cache()
+
     return df
