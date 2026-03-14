0c80f7715a1786793f80164deba607dd0b19bfa0
Sowdeshwar Survesha Kumaar <59279838+Sowdeshwar-99@users.noreply.github.com>
2026-03-14 20:33:58 +1000

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,10 +1,10 @@
 from pipeline.ingestion import create_spark, load_ads_data
-from pipeline.preprocessing import clean_ads, tokenize_ads
+from pipeline.preprocessing import clean_ads, split_words
 from pipeline.keyword_analysis import keyword_frequency
 from pipeline.spend_analysis import top_investors
 from pipeline.cpm_analysis import calculate_cpm
 
-DATA_PATH = "hdfs:///data/ProjectDatasetFacebookAU/*"
+DATA_PATH = "hdfs:///data/ProjectDatasetFacebookAU/adsAU/*"
 
 def main():
 
@@ -14,7 +14,7 @@ def main():
 
     ads_clean = clean_ads(df)
 
-    words = tokenize_ads(ads_clean)
+    words = split_words(ads_clean)
 
     word_counts = keyword_frequency(words)
 
@@ -22,9 +22,9 @@ def main():
 
     cpm_df = calculate_cpm(ads_clean)
 
-    word_counts.show()
-    investors.show()
-    cpm_df.show()
+    word_counts.show(20)
+    investors.show(20)
+    cpm_df.show(20)
 
 if __name__ == "__main__":
     main()
